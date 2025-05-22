12cf8e319a9960a4ffc15d01040134549129ab30
lfichouledantec <lfichou.ledantec@gmail.com>
2025-05-22 23:15:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <h1>hello</h1>
+<?php
+$link = mysqli_connect('localhost', 'root', 'root');  // adapte le mot de passe si besoin
+if (!$link) {
+    die('Erreur de connexion (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+echo 'Connexion MySQL réussie via PHP';
+mysqli_close($link);
+?>
